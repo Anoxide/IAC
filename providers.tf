f83483838bd160926f048604f2cba44d8dd73705
Anoxide <julien.roy06@icloud.com>
2024-10-25 13:29:34 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,8 +5,4 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = "76bb3926-78f3-4cfd-aed6-c8b63b0e2b0a"
-  client_id       = "a49d0228-2c79-4cc7-b4cf-998643f6dfe0"
-  client_secret   = "QAM8Q~0QteLhZnHHR4FdhLyBMxt8aELCvEjsAbSx"
-  tenant_id       = "424b56f4-c9d7-454a-a034-33cb8dd34ead"
 }
