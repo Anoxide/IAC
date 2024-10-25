df31b071294e59ead8ae7bac45fabf537f6c3a4a
Anoxide <julien.roy06@icloud.com>
2024-10-25 13:50:25 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,8 +5,8 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = "$(secrets.SUBSCRIPTION_ID)"
-  client_id       = "$(secrets.CLIENT_ID)"
-  client_secret   = "$(secrets.CLIENT_SECRET)"
-  tenant_id       = "$(secrets.TENANT_ID)"
+  subscription_id = "${secrets.SUBSCRIPTION_ID}"
+  client_id       = "${secrets.CLIENT_ID}"
+  client_secret   = "${secrets.CLIENT_SECRET}"
+  tenant_id       = "${secrets.TENANT_ID}"
 }
